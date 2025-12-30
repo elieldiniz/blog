633709881f0bb6cd357f5870e0028c1a2486732a
Elieldiniz <elieldiniz1@outlook.com>
2025-12-29 23:30:28 -0400

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','PrincipalController@principal')->name('site.index');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobreNos');
+Route::get('/login', function(){return 'login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){
+    return 'clientes';
+    })->name('app.clientes');
+
+    Route::get('/fornecedores', function(){
+        return 'fornecedores';
+    })->name('app.fornecedores');
+
+    Route::get('/produtos', function(){
+        return 'produtos';
+    })->name('app.produtos');
+});
